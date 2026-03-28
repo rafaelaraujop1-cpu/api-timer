2a01487b8e53b512f1a5705bd358ffba23e5b543
rafaelaraujop1-cpu <rafael.araujop1@gmail.com>
2026-03-28 12:11:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,46 +1,26 @@
-const express = require("express");
-const { Pool } = require("pg");
-const cors = require("cors");
+Tá errado. 
+Calcule as horas respeitando o horário útil de trabalho:
 
-const app = express();
-app.use(cors());
+Segunda a Quinta: 07:30 as 12:00 e 13:0 as 17:30
+Sexta: 07:30 as 12:00 e 13:00 as 16:30
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: false
-});
+Vou te dar os resultados da Api e os valores que preciso que aparece no timer:
 
-app.get("/api/timer", async (req, res) => {
-  try {
-    const result = await pool.query(`
-      SELECT
-        pedidovendaid::text AS pedido,
-        nome_cadcftv AS cliente,
-        to_char(dt_reserva AT TIME ZONE 'America/Sao_Paulo', 'YYYY-MM-DD HH24:MI:SS') AS inicio,
-        to_char(primeira_conferencia, 'YYYY-MM-DD HH24:MI:SS') AS primeira_conferencia,
-        to_char(ultima_conferencia, 'YYYY-MM-DD HH24:MI:SS') AS ultima_conferencia,
-        tempo_util_formatado,
-        situacao
-      FROM vw_pedidos_timer
-      WHERE dt_reserva IS NOT NULL
-      ORDER BY dt_reserva ASC
-      LIMIT 100
-    `);
+Quando for tiver a data ultima_conferencia preenchida:
 
-    res.json(result.rows);
-  } catch (err) {
-    console.error("ERRO TIMER:", err);
-    res.status(500).json({
-      erro: err.message,
-      detalhe: err.code || null
-    });
-  }
-});
+"pedido":"377302","cliente":"A M DE SA COMERCIO DE METAIS","inicio":"2026-03-26 15:51:50","primeira_conferencia":"2026-03-27 08:20:45","ultima_conferencia":"2026-03-27 08:36:21","tempo_util_formatado":"0d 09:38:10","situacao":"FATURADO"}
 
-app.get("/", (req, res) => {
-  res.send("API funcionando");
-});
+Inicio: 026-03-26 15:51:507
+Ultima Conferencia:  2026-03-27 08:36:21
 
-app.listen(10000, () => {
-  console.log("API rodando na porta 10000");
-});
+15:51:50 → 17:30:00 = 01:38:10
+07:30:00 → 08:36:21 = 01:06:21
+Resultado do timer: 02:44:31
+
+Quando não tiver a data ultima_conferencia preenchida:
+
+Calcular o timer baseado no último horário útil:
+Inicio: 026-03-26 07:30:00
+
+Se hoje fosse dia 27/03/2026 sexta feira as 16:00 ele deveria preencher o timer e continuar contando até chegar 16:30, timer ficaria sim:
+1d 07:30
